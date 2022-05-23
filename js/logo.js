a36e86d3e7dcdc3c9c912a9dcb143ced9d57ef5a
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-05-22 22:27:12 -0300

    Add files via upload

--- js/logo.js
+++ js/logo.js
@@ -1,7 +1,7 @@
 const item = document.querySelector(".logo");
 
 item.animate(
-  [{ transform: "translateX(-7rem)" }, { transform: "translateY(0rem)" }],
+  [{ transform: "translateX(-4rem)" }, { transform: "translateY(0rem)" }],
   {
     duration: 4000,
     easing: "ease-in-out",
